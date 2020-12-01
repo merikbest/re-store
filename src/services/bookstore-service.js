68a6188e879c58e7438ade2f6817721248cbb3f8
merik <merikbest2015@gmail.com>
2020-12-01 23:10:22 +0300

    Added pages and services.

---
+++ src/services/bookstore-service.js
@@ -0,0 +1,17 @@
+export default class BookstoreService {
+    getBooks() {
+        return [
+            {
+                id: 1,
+                title: "Java: The Complete Reference",
+                author: "Herbert Schildt"
+            },
+            {
+                id: 2,
+                title: "Thinking In Java",
+                author: "Bruce Eckel"
+            }
+
+        ];
+    }
+}
---
+++ src/services/index.js
@@ -0,0 +1,5 @@
+import withBookstoreService from "../components/hoc/with-bookstore-service";
+
+export {
+    withBookstoreService
+};
