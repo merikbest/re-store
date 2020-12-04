dd53899b0e914dcb10433dfacaa5d75ec21e05c5
merik <merikbest2015@gmail.com>
2020-12-04 23:34:05 +0300

    Fixed reducer.

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -49,12 +49,8 @@ class BookListContainer extends Component {
     }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        books: state.books,
-        loading: state.loading,
-        error: state.error
-    };
+const mapStateToProps = ({bookList: {books, loading, error}}) => {
+    return {books, loading, error};
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,98 +1,11 @@
-const initialState = {
-    books: [],
-    loading: true,
-    error: null,
-    cartItems: [],
-    orderTotal: 220
-};
+import updateBookList from "./book-list";
+import updateShoppingCart from "./shopping-cart";
 
-const updateCartItems = (cartItems, item, idx) => {
-    // неправильно. push - изменяет состояние, а в redux этого делать нельзя
-    // так как метод push модифицирует массив
-    // state.cartItems.push(newItem);
-    if (idx === -1) {
-        return [
-            ...cartItems,
-            item
-        ];
-    }
-
-    return [
-        ...cartItems.slice(0, idx),
-        item,
-        ...cartItems.slice(idx + 1),
-    ];
-};
-
-const updateCartItem = (book, item = {}) => {
-    const {
-        id = book.id,
-        count = 0,
-        title = book.title,
-        total = 0} = item;
-
+const reducer = (state, action) => {
     return {
-        id,
-        title,
-        count: count +1,
-        total: total + book.price
+        bookList: updateBookList(state, action),
+        shoppingCart: updateShoppingCart(state, action)
     };
-
-    // if (item) {
-    //     return {
-    //         ...item,
-    //         count: item.count + 1,
-    //         total: item.total + book.price
-    //     };
-    // } else {
-    //     return {
-    //         id: book.id,
-    //         title: book.title,
-    //         count: 1,
-    //         total: book.price
-    //     };
-    // }
-}
-
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "FETCH_BOOKS_REQUEST":
-            return {
-                ...state,
-                books: [],
-                loading: true,
-                error: null
-            };
-        case "FETCH_BOOKS_SUCCESS":
-            return {
-                ...state,
-                books: action.payload,
-                loading: false,
-                error: null
-            };
-        case "FETCH_BOOKS_FAILURE":
-            return {
-                ...state,
-                books: [],
-                loading: false,
-                error: action.payload
-            };
-        case "BOOK_ADDED_TO_CART":
-            const bookId = action.payload;
-            const book = state.books.find((book) => book.id === bookId);
-            const itemIndex = state.cartItems.findIndex(({id}) => id === bookId);
-            const item = state.cartItems[itemIndex];
-
-            const newItem = updateCartItem(book, item);
-
-            return {
-                ...state,
-                cartItems: updateCartItems(state.cartItems, newItem, itemIndex)
-            };
-
-        default:
-            return state;
-    }
 };
 
 export default reducer;
